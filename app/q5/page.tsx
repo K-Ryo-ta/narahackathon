328befc846f615b81fb68d6aada6be2bdd5b8276
K-Ryo-ta <studyprograming51@gmail.com>
2024-03-19 02:53:48 +0900

    戻す

--- app/q5/page.tsx
+++ app/q5/page.tsx
@@ -9,7 +9,7 @@ import { aggregateStats, getData, saveResponse } from "@/lib/firebase";
 import { isSupported } from "firebase/analytics";
 import { getAnalytics } from "firebase/analytics";
 import { DocumentData } from "firebase/firestore";
-import { useStateContext } from "../stateManegement";
+import StateContext from "../stateManegement";
 
 const Page5 = () => {
     const router = useRouter();
@@ -17,7 +17,7 @@ const Page5 = () => {
     const [data, setData] = useState<DocumentData | null>(null);
     const [docId, setDocId] = useState<string | null>(null);
     const [progress, setProgress] = useState<number>(80);
-    const { state, setState } = useStateContext();
+    const stateInfo = useContext(StateContext);
 
     useEffect(() => {
         if (docId) {
@@ -25,33 +25,22 @@ const Page5 = () => {
         }
     }, [docId]);
 
-    const IsBrank = (props: string) => {
-        // if (props == "/^\s*$/") return false;
-        // else return true;
-        return /^\s*$/.test(props);
-    };
-
     const NextRouteHandleClick = async (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
         event.preventDefault();
-        console.log(IsBrank(state.q5));
-        if (state.q5) {
-            if (!IsBrank(state.q5)) {
-                const docID = await saveResponse("q5", answer);
-                if (docID) {
-                    setState(prevState => ({
-                        ...prevState,
-                        docRefID4: docID,
-                    }));
-                    setDocId(docID);
-                }
-                // 回答が選択されている場合は次のページに遷移
-                console.log(state.q5);
-                router.push('/finish');
-            } else {
-                alert('文字を入力してください');
+        if (answer) {
+            const docID = await saveResponse("q5", answer);
+            if (docID) {
+                stateInfo.docRefID5 = docID
             }
+            console.log(stateInfo.docRefID5);
+            if (stateInfo.docRefID5) {
+                setDocId(stateInfo.docRefID5);
+            }
+            // 回答が選択されている場合は次のページに遷移
+            router.push('/finish');
         } else {
-            alert('回答を選択してください');
+            // 回答が選択されていない場合はアラートを表示
+            alert('回答を選択してください。');
         }
     }
 
@@ -63,19 +52,11 @@ const Page5 = () => {
     const handleAnswer = (selectedAnswer: string) => {
         setAnswer(selectedAnswer);
         setProgress(100);
-        setState(prevState => ({
-            ...prevState,
-            q5: selectedAnswer,
-        }));
+        stateInfo.q5 = selectedAnswer;
     }
 
     const handleOtherAnswer = (event: React.ChangeEvent<HTMLInputElement>) => {
         setAnswer(event.target.value);
-        setState(prevState => ({
-            ...prevState,
-            q5: event.target.value,
-        }));
-        setProgress(100);
     }
 
     useEffect(() => {
